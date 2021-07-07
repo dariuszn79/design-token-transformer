7cf19b9b3ae02ab786e691a004ba13430cf27d33
Automated Publisher <actions@users.noreply.github.com>
2021-07-07 14:49:05 +0000

    Automated publish: Wed Jul  7 14:49:05 UTC 2021 d882213a3fa8a176df2116116e1aaa8c9ba1de01

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -3,11 +3,53 @@
 // StyleDictionaryColor.swift
 //
 // Do not edit directly
-// Generated on Mon, 05 Jul 2021 10:15:22 GMT
+// Generated on Wed, 07 Jul 2021 14:49:04 GMT
 //
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    
+    public static let blackBlack05 = UIColor(red: 0.200, green: 0.200, blue: 0.200, alpha:0.05)
+    public static let blackBlack100 = UIColor(red: 0.290, green: 0.290, blue: 0.298, alpha:1)
+    public static let blackBlack25 = UIColor(red: 0.200, green: 0.200, blue: 0.200, alpha:0.25)
+    public static let blackBlack50 = UIColor(red: 0.200, green: 0.200, blue: 0.200, alpha:0.5)
+    public static let blackBlack75 = UIColor(red: 0.290, green: 0.290, blue: 0.290, alpha:0.75)
+    public static let functionalError = UIColor(red: 1.000, green: 0.384, blue: 0.251, alpha:1)
+    public static let functionalWarning = UIColor(red: 1.000, green: 0.788, blue: 0.251, alpha:1)
+    public static let gradientsSkyBlueGradientStops0Color = UIColor(red: 0.039, green: 0.059, blue: 0.961, alpha:1)
+    public static let gradientsSkyBlueGradientStops1Color = UIColor(red: 0.000, green: 0.588, blue: 1.000, alpha:1)
+    public static let gradientsSkyPurpleGradientStops0Color = UIColor(red: 1.000, green: 0.000, blue: 0.647, alpha:1)
+    public static let gradientsSkyPurpleGradientStops1Color = UIColor(red: 0.431, green: 0.000, blue: 1.000, alpha:1)
+    public static let gradientsSkyRainbowGradientStops0Color = UIColor(red: 1.000, green: 0.620, blue: 0.000, alpha:1)
+    public static let gradientsSkyRainbowGradientStops1Color = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha:1)
+    public static let gradientsSkyRainbowGradientStops2Color = UIColor(red: 0.710, green: 0.000, blue: 0.490, alpha:1)
+    public static let gradientsSkyRainbowGradientStops3Color = UIColor(red: 0.129, green: 0.259, blue: 0.612, alpha:1)
+    public static let gradientsSkyRainbowGradientStops4Color = UIColor(red: 0.000, green: 0.443, blue: 1.000, alpha:1)
+    public static let gradientsSkyRedGradientStops0Color = UIColor(red: 0.961, green: 0.392, blue: 0.000, alpha:1)
+    public static let gradientsSkyRedGradientStops1Color = UIColor(red: 1.000, green: 0.039, blue: 0.314, alpha:1)
+    public static let gradientsSkyRedGradientStops2Color = UIColor(red: 1.000, green: 0.000, blue: 0.647, alpha:1)
+    public static let primaryPrimary05 = UIColor(red: 0.000, green: 0.447, blue: 0.788, alpha:0.05)
+    public static let primaryPrimary100 = UIColor(red: 0.000, green: 0.447, blue: 0.788, alpha:1)
+    public static let primaryPrimary25 = UIColor(red: 0.000, green: 0.447, blue: 0.788, alpha:0.25)
+    public static let primaryPrimary50 = UIColor(red: 0.000, green: 0.447, blue: 0.788, alpha:0.5)
+    public static let primaryPrimary75 = UIColor(red: 0.000, green: 0.447, blue: 0.788, alpha:0.75)
+    public static let secondarySecondary100 = UIColor(red: 0.000, green: 0.447, blue: 0.788, alpha:1)
+    public static let secondarySecondary25 = UIColor(red: 0.000, green: 0.447, blue: 0.788, alpha:0.25)
+    public static let secondarySecondary50 = UIColor(red: 0.000, green: 0.447, blue: 0.788, alpha:0.5)
+    public static let tertiaryTertiary100 = UIColor(red: 0.961, green: 0.231, blue: 0.800, alpha:1)
+    public static let tertiaryTertiary25 = UIColor(red: 0.961, green: 0.231, blue: 0.800, alpha:0.25)
+    public static let tertiaryTertiary50 = UIColor(red: 0.961, green: 0.231, blue: 0.800, alpha:0.5)
+    public static let whiteWhite0 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha:1)
+    public static let whiteWhite05 = UIColor(red: 0.988, green: 0.988, blue: 0.988, alpha:0.05)
+    public static let whiteWhite100 = UIColor(red: 0.988, green: 0.988, blue: 0.988, alpha:1)
+    public static let whiteWhite25 = UIColor(red: 0.988, green: 0.988, blue: 0.988, alpha:0.25)
+    public static let whiteWhite50 = UIColor(red: 0.988, green: 0.988, blue: 0.988, alpha:0.5)
+    public static let whiteWhite75 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha:0.75)
+    public static let elevationBaseColor = rgba(51, 51, 51, 0.05)
+    public static let elevationBoxSmlColor = rgba(51, 51, 51, 0.12)
+    public static let elevationInner0Color = rgba(0, 0, 0, 0.1)
+    public static let elevationInner1Color = rgba(0, 0, 0, 0.05)
+    public static let elevationLargeColor = rgba(51, 51, 51, 0.05)
+    public static let elevationMedColor = rgba(0, 0, 0, 0.05)
+    public static let elevationOutlineColor = rgba(1, 102, 225, 0.15)
 }
--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -3,14 +3,56 @@
 // StyleDictionaryColor.h
 //
 // Do not edit directly
-// Generated on Mon, 05 Jul 2021 10:15:22 GMT
+// Generated on Wed, 07 Jul 2021 14:49:04 GMT
 //
 
 #import <UIKit/UIKit.h>
 
 
 typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
-
+ColorPrimaryPrimary100,
+ColorPrimaryPrimary75,
+ColorPrimaryPrimary50,
+ColorPrimaryPrimary25,
+ColorPrimaryPrimary05,
+ColorSecondarySecondary100,
+ColorSecondarySecondary50,
+ColorSecondarySecondary25,
+ColorTertiaryTertiary100,
+ColorTertiaryTertiary50,
+ColorTertiaryTertiary25,
+ColorWhiteWhite100,
+ColorWhiteWhite75,
+ColorWhiteWhite50,
+ColorWhiteWhite25,
+ColorWhiteWhite05,
+ColorWhiteWhite0,
+ColorBlackBlack100,
+ColorBlackBlack75,
+ColorBlackBlack50,
+ColorBlackBlack25,
+ColorBlackBlack05,
+ColorFunctionalError,
+ColorFunctionalWarning,
+ColorGradientsSkyRainbowGradientStops0Color,
+ColorGradientsSkyRainbowGradientStops1Color,
+ColorGradientsSkyRainbowGradientStops2Color,
+ColorGradientsSkyRainbowGradientStops3Color,
+ColorGradientsSkyRainbowGradientStops4Color,
+ColorGradientsSkyRedGradientStops0Color,
+ColorGradientsSkyRedGradientStops1Color,
+ColorGradientsSkyRedGradientStops2Color,
+ColorGradientsSkyBlueGradientStops0Color,
+ColorGradientsSkyBlueGradientStops1Color,
+ColorGradientsSkyPurpleGradientStops0Color,
+ColorGradientsSkyPurpleGradientStops1Color,
+EffectElevationBaseColor,
+EffectElevationMedColor,
+EffectElevationLargeColor,
+EffectElevationBoxSmlColor,
+EffectElevationInner0Color,
+EffectElevationInner1Color,
+EffectElevationOutlineColor
 };
 
 @interface StyleDictionaryColor : NSObject
