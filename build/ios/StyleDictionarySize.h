97f3adfe9fe02f6993e8e03581886f276450cd00
Automated Publisher <actions@users.noreply.github.com>
2022-04-28 16:44:26 +0000

    Automated publish: Thu Apr 28 16:44:26 UTC 2022 b42e03e855769211433bb31555142f9c5de8a95d

--- build/ios/StyleDictionarySize.h
+++ build/ios/StyleDictionarySize.h
@@ -2,7 +2,7 @@
 // StyleDictionarySize.h
 //
 // Do not edit directly
-// Generated on Thu, 28 Apr 2022 16:42:10 GMT
+// Generated on Thu, 28 Apr 2022 16:44:26 GMT
 //
 
 #import <Foundation/Foundation.h>
